96915b883f84bfe18e91a01bf54c86c78a91c9ac
Noor Piash <noorpiash1234@gmail.com>
2025-11-08 18:19:56 +0600

    bookcontext

--- client/src/context/BookContext.jsx
+++ client/src/context/BookContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const BookContext = createContext()
@@ -11,33 +11,85 @@ export const BookProvider = ({ children }) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+
+
+
+    const [filters, setFilters] = useState({
+        page: 1,
+        limit: 8,
+        genre: "",
+        minYear: "",
+        maxYear: "",
+        author: "",
+        maxPrice: "",
+        minPrice: "",
+        sortBy: "title",
+        order: "asc",
+        search: "",
+
+    })
+
+    const [pagination, setPagination] = useState({
+        totalBooks: 0,
+        currentPage: 1,
+        totalpages: 1
+
+    })
+
+
+
+
+
+
+
+
+    const fetchBooks = useCallback(async () => {
+        try {
+            setLoading(true)
+            setError(null)
+
+
+            const params = new URLSearchParams();
+            Object.entries(filters).forEach(([key, value]) => {
+                if (value !== "") {
+                    params.append(key, value)
+                }
+            })
+
+            const response = await axios.get(`http://localhost:3000/books?${params}`)
+            setBooks(response.data.books)
+            setPagination({
+                totalBooks: response.data.totalBooks,
+                currentPage: response.data.currentPage,
+                totalpages: response.data.totalpages
+            })
+
+
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false)
+        }
+    }, [filters])
+
+    const clearCurrentBook = useCallback(() => {
+        setBooks(null);
+    }, [])
+
+    useEffect(() => {
+        fetchBooks();
+    }, [filters,])
+
     const value = {
         books,
         currentBook,
         loading,
-        error
+        error,
+        filters,
+        pagination,
+        fetchBooks,
+        clearCurrentBook,
     }
-
-
-
-    const fetchBooks = async () => {
-        try {
-            setLoading(true);
-            setError(null);
-
-            const response = await axios.get("http://localhost:3000/books")
-            console.log(response)
-
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    useEffect(() => {
-        fetchBooks();
-    }, [])
-
 
 
 
